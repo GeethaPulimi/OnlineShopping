56ef71fd6b74e7cb440017f4bbf5858580bff583
Geetha Pulimi <p.geetha413@gmail.com>
2018-03-14 16:43:54 -0500

    My project setup completed sussesfully.

---
+++ shoppingBackend/src/main/java/org/geetha/shoppingBackend/App.java
@@ -0,0 +1,13 @@
+package org.geetha.shoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
